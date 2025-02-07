fd5945dcb1b46f8a5550be84d2ec6edd044ef807
mtc97 <43764347+mtc97@users.noreply.github.com>
2025-02-07 13:40:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,9 @@ port_number = int(os.environ.get("APP_PORT", 5153))
 def health_check():
     return "ok"
 
+class Token(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    token_value = db.Column(db.String(256), unique=True, nullable=False)
 
 @app.route("/readiness_check")
 def readiness_check():
